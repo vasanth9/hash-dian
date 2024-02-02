6f2999ac6f8e8fe99c3b3d0aa4ce25d8535edb18
Vasanth Kumar Cheepurupalli <cheepurupalli.vasanthkumar@gmail.com>
2024-02-02 19:35:49 +0530

    "version 1 complete"

--- main.ts
+++ main.ts
@@ -1,78 +1,98 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { App, Editor, MarkdownView, Modal, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { load as yamlToJson } from 'js-yaml';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface HashDianPluginSettings {
+	APIToken: string;
+	PublicationID: string
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const ERROR_CODES = {
+	INVALID_METADATA: "Invalid MetaData",
+	INVALID_DATA: "Invalid Data",
+	NO_ERROR: "",
+	ERROR_API_MODAL: "👾 Error While Publishing the Blog"
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const BasicBlogTemplate = `---
+title:
+subtitle:
+coverImageURL.coverImageOptions:
+tags:
+  -
+---
+`;
+
+const AdvancedBlogTemplate = `---
+title:
+subtitle:
+publishedAt: ${new Date().toISOString()}
+coverImageURL.coverImageOptions:
+isCoverAttributionHidden.coverImageOptions:
+coverImageAttribution.coverImageOptions:
+coverImagePhotographer.coverImageOptions:
+stickCoverToBottom.coverImageOptions:
+slug:
+originalArticleURL:
+tags:
+  -
+disableComments: false
+metaTags.title:
+metaTags.description:
+metaTags.image:
+publishAs:
+seriesId:
+scheduled.settings: false
+enableTableOfContent.settings: false
+slugOverridden.settings: false
+isNewsletterActivated.settings: false
+delisted.settings: false
+coAuthors:
+  -
+---
+`;
+
+const DEFAULT_SETTINGS: HashDianPluginSettings = {
+	APIToken: '',
+	PublicationID: ''
+}
+
+let ErrorData: any = null;
+let SuccessData: any = null;
+
+export default class HashDianPlugin extends Plugin {
+	settings: HashDianPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// publish to hashnode command
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+			id: 'publish-to-hashnode',
+			name: 'Publish to Hashnode',
+			editorCallback: () => { handleEditorCallBack(this.app, this.settings); }
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
+
+		//basic hashnode blog template
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'basic-hashnode-blog-template',
+			name: 'Basic Hashnode Blog Template',
 			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+				editor.setLine(0, BasicBlogTemplate + editor.getLine(0));
 			}
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
+		//advanced hashnode blog template
+		this.addCommand({
+			id: 'advanced-hashnode-blog-template',
+			name: 'Advanced Hashnode Blog Template',
+			editorCallback: (editor: Editor, view: MarkdownView) => {
+				editor.setLine(0, AdvancedBlogTemplate + editor.getLine(0));
 			}
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
 		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
@@ -91,43 +111,208 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
+const handleEditorCallBack = (app: App, settings: HashDianPluginSettings) => {
+	const view = app.workspace.getActiveViewOfType(MarkdownView);
+	const { config, contentMarkdown, error } = parseDataForPublication(view?.data);
+	if (error === ERROR_CODES.NO_ERROR) {
+		// publish the article
+		publishArticle(settings, app, { ...config, contentMarkdown })
+	} else {
+		// show err modal
+		const response = {
+			errors: {
+				error
+			}
+		}
+		ErrorData = response;
+		new ErrorAPIModal(app).open();
+	}
+}
+const parseDataForPublication = (data: string | undefined): any => {
+	try {
+		if (data) {
+			const lines: string[] = data?.split("\n");
+			const numberOfLines: number = lines.length;
+
+			// get metaDataIndices 
+			const indexes = [];
+			for (let i = 0; i < numberOfLines; i++) {
+				if (/^---/.test(lines[i])) {
+					indexes.push(i);
+				}
+				if (indexes.length === 2) {
+					break;
+				}
+			}
+			if (indexes.length !== 2) {
+				// implement error functionality
+				return { metadata: "", content: "", error: ERROR_CODES.INVALID_METADATA }
+			}
+
+			const metaDataLines = lines.slice(indexes[0] + 1, indexes[1]);
+			const contentLines = lines.slice(indexes[1] + 1);
+
+			let config: any = yamlToJson(metaDataLines.join("\n"));
+
+			// remove null values
+			config = JSON.parse(JSON.stringify(config, (key, value) => {
+				return (value === null ? undefined : value);
+			}));
+
+			// convert child.parent to object parent { child: }
+			config = Object.entries(config).reduce((newObj: any, [key, value]) => {
+				const keys = key.split('.');
+				if (keys.length === 1) {
+					newObj[key] = value;
+				} else {
+					newObj[keys[1]] = newObj[keys[1]] || {};
+					newObj[keys[1]][keys[0]] = value;
+				}
+				return newObj;
+			}, {});
+
+			if (typeof config.tags === 'string') {
+				config["tags"] = config?.tags?.split(/, \s*/)
+			}
+
+			config.tags = config?.tags?.map((tag: any) => { return { slug: tag, name: tag } }) || [];
+
+			return {
+				config,
+				contentMarkdown: contentLines.join("\n"),
+				error: ERROR_CODES.NO_ERROR
+			}
+
+
+		}
+	}
+	catch (err) {
+		return { metadata: "", content: "", error: err }
+	}
+
+	return { metadata: "", content: "", error: ERROR_CODES.INVALID_DATA }
+}
+
+const publishArticle = (settings: HashDianPluginSettings | null, app: App, data: any) => {
+
+	const API_URL = 'https://gql.hashnode.com';
+
+	const API_QUERY = 'mutation createPost($input: PublishPostInput!) {publishPost(input: $input) { post { title url } } } ';
+
+	const input = {
+		...data,
+		publicationId: data?.PublicationID || settings?.PublicationID,
+	};
+
+	fetch(API_URL, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: settings?.APIToken || ""
+		},
+		body: JSON.stringify({
+			query: API_QUERY,
+			variables: { input },
+		}),
+	})
+		.then(res => res.json())
+		.then(res => {
+			console.log(res);
+			if (res?.errors?.length) {
+				ErrorData = res;
+				new ErrorAPIModal(app).open();
+			}
+			else {
+				const post = res?.data?.publishPost?.post;
+
+				if (post) {
+					SuccessData = post;
+					new SuccessAPIModal(app).open();
+				}
+			}
+		})
+}
+
+class SuccessAPIModal extends Modal {
 	constructor(app: App) {
 		super(app);
 	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	async onOpen() {
+		const { contentEl, titleEl } = this;
+		const { title, url } = SuccessData;
+		if (title.length) {
+			titleEl.setText("Your Blog " + title + " has been Published 🎉");
+		}
+		if (url.length) {
+			contentEl.setText(url);
+			contentEl.setCssProps({ color: "green", cursor: "pointer" })
+			contentEl.addEventListener("click", () => {
+				window.open(url, "_blank");
+			})
+		}
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
+		SuccessData = null;
+		contentEl.empty();
+	}
+}
+
+class ErrorAPIModal extends Modal {
+	constructor(app: App) {
+		super(app);
+	}
+	async onOpen() {
+		const { contentEl, titleEl } = this;
+		titleEl.setText(ERROR_CODES.ERROR_API_MODAL);
+		const errors = ErrorData.errors
+
+		if (errors.length) {
+			for (let i = 0; i < errors.length; i++) {
+				contentEl.setText(JSON.stringify(errors[i], null, 2));
+			}
+		}
+	}
+
+	onClose() {
+		const { contentEl } = this;
+		ErrorData = null;
 		contentEl.empty();
 	}
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+	plugin: HashDianPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: HashDianPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Hashnode Personal Access Token')
+			.setDesc('Personal Access Token is like a private key to your Hashnode account. You can use a personal access token to interact with your Hashnode account using our APIs. Never disclose your access tokens and always keep them private. You can revoke the generated tokens any time.')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your Hashnode Personal Access Token')
+				.setValue(this.plugin.settings.APIToken)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.APIToken = value;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('Hashnode Personal Publication Id')
+			.setDesc('The ID of publication the post belongs to.')
+			.addText(text => text
+				.setPlaceholder('Enter your Hashnode Personal Publication Id')
+				.setValue(this.plugin.settings.PublicationID)
+				.onChange(async (value) => {
+					this.plugin.settings.PublicationID = value;
 					await this.plugin.saveSettings();
 				}));
 	}
